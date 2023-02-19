72c75ac4c297ee2278f70cdb86ec6a3013edcca8
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2023-02-19 17:14:04 +0100

    avancement prologTerm

--- APS_ocaml/prologTerm.ml
+++ APS_ocaml/prologTerm.ml
@@ -14,12 +14,48 @@ let rec print_expr e =
       ASTNum n -> Printf.printf"num(%d)" n
     | ASTId x -> Printf.printf"id(%s)" x
     | ASTApp(e, es) -> (
-	Printf.printf"app(";
-	print_expr e;
-	Printf.printf",[";
-	print_exprs es;
-	Printf.printf"])"
-      )
+      Printf.printf"app(";
+      print_expr e;
+      Printf.printf",[";
+      print_exprs es;
+      Printf.printf"])"
+    )
+    | ASTIf(cond,vrai,faux) -> (
+      Printf.printf "if";
+      Printf.printf "(";
+      print_expr cond;
+      Printf.printf ",";
+      print_expr vrai;
+      Printf.printf ",";
+      print_expr faux;
+      Printf.printf ")";
+    )
+    | ASTAnd(e1,e2) -> (
+      Printf.printf "and";
+      Printf.printf "(";
+      print_expr e1;
+      Printf.printf ",";
+      print_expr e2;
+      Printf.printf ")";
+    )
+    | ASTFun(args,e) -> (
+      Printf.printf "abs";
+      Printf.printf "(";
+      Printf.printf "[";
+      print_args args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_expr e;
+      Printf.printf ")";
+    )
+    | ASTOr(e1,e2) -> (
+      Printf.printf "or";
+      Printf.printf "(";
+      print_expr e1;
+      Printf.printf ",";
+      print_expr e2;
+      Printf.printf ")";
+    )
       
 and print_exprs es =
   match es with
@@ -47,7 +83,68 @@ let rec print_cmds cs =
   match cs with
       c::[] -> print_cmd c
     | _ -> failwith "not yet implemented"
-	
+
+let rec print_type t=
+  match t with 
+  ASTTypBool -> ????
+  | ASTTypInt -> ????
+  | ASTTypFleche(typ,typs) -> (
+      print_type typ;
+      Printf.printf ",";
+      ????? typs;
+    )
+
+let print_arg arg = 
+  match arg with
+    ASTArg(nom, typ) -> (
+      Printf.printf "(";
+      Printf.printf "%s" nom;
+      Printf.printf ",";
+      print_type typ;
+      Printf.printf ")"
+    )
+
+let print_def d = 
+  match d with
+    ASTDefConst(nom, type, e) -> (
+      Printf.printf "const";
+      Printf.printf "(";
+      Printf.printf "%s" nom;
+      Printf.printf ",";
+      print_type type;
+      Printf.printf ",";
+      print_expr e;
+      Printf.printf ")";
+    )
+  | ASTDefFun(nom, type, args, e) -> (
+      Printf.printf "fun";
+      Printf.printf "(";
+      Printf.printf "%s" nom;
+      Printf.printf ",";
+      print_type type;
+      Printf.printf ",";
+      Printf.printf "[";
+      ?????? args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_expr e;
+      Printf.printf ")";
+    )
+  | ASTDefFunRec(nom, type, args, e) -> (
+      Printf.printf "funrec";
+      Printf.printf "(";
+      Printf.printf "%s" nom;
+      Printf.printf ",";
+      print_type type;
+      Printf.printf ",";
+      Printf.printf "[";
+      ???? args;
+      Printf.printf "]";
+      Printf.printf ",";
+      print_expr e;
+      Printf.printf ")";
+  )
+
 let print_prog p =
   Printf.printf("prog([");
   print_cmds p;
